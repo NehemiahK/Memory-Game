65926ae2883cafbb33bb866ff01cbe08519ba53a
nehemiahk <nlk613@yahoo.com>
2017-12-21 14:57:16 +0200

    made everything rick and morty stlye. allowed user to start a new game. fixed issue where user could flip over already turned tiles

--- memory/memory.js
+++ memory/memory.js
@@ -1,4 +1,5 @@
 function gameStart(){
+
     function shuffle(array) {
         let counter = array.length;
 
@@ -31,74 +32,87 @@ function gameStart(){
         for (var i=0; i<4;i++){
             var img = document.createElement("img");
             img.className="card";
-            var att = document.createAttribute("src");
-            var alt = document.createAttribute("alt");
-            var id = document.createAttribute("id");
-            alt.value=shuffled[valscounter];
-            att.value= "texture.jpg";
-            id.value=valscounter;
-            img.setAttributeNode(att);
-            img.setAttributeNode(alt);
-            img.setAttributeNode(id);
+
+            img.setAttribute("src","texture.jpg");
+            img.setAttribute("id",valscounter);
+            img.setAttribute("alt",shuffled[valscounter]);
+
             document.getElementById(x).appendChild(img);
             valscounter++;
         }
         counter++;
     }
+
+
+    var checkVal = []; // holds the past two values to compare them.
+    var checkId = []; // hold the past two ids to flip them over.
+    var badGuess=0;
+    var correct=[];
+
+    function checkCard(){
+
+        //var altVal = this.alt;
+        if (this.id != checkId[0] && correct.includes(this.alt) == false){ // make sure they dont click the same card twice.
+
+            checkVal.push(this.alt);
+            checkId.push(this.id);
+
+            if (checkVal.length==1){
+                this.src = this.alt + ".jpg";
+
+            }
+            else if (checkVal.length ==2){
+                this.src = this.alt + ".jpg";
+            }
+
+            var card1 =checkVal[0];
+            var card2 =checkVal[1];
+
+            if (checkVal.length==2){
+
+                if (card1!=card2){
+
+                    var myVar = setTimeout(myTimer, 1400); // delay feature for turning back over.
+                            function myTimer() {
+                                document.getElementById(checkId[0]).src = "texture.jpg";
+                                document.getElementById(checkId[1]).src = "texture.jpg";
+
+                                badGuess++;
+                                document.getElementById('incorrect').innerHTML="Incorrect: " + badGuess;
+                                checkId=[];
+                                checkVal=[];
+                            }
+
+                }
+                else{
+                    correct.push(card1);
+                    checkId=[];
+                    checkVal=[];
+                }
+            }
+        }
+        if (correct.length==6){
+            alert("you win");
+        }
+    }
+    /*assigning the event listeners to the cards */
+
+    var cards = document.getElementsByClassName("card");
+    for (var x=0; x<cards.length; x++){
+        cards[x].addEventListener("click", checkCard);
+    }
+
 }
 
 gameStart();
 
 
-var checkVal = []; // holds the past two values to compare them.
-var checkId = []; // hold the past two ids to flip them over.
+function newBoard(){
+    document.getElementById("row1").innerHTML="";
+    document.getElementById("row3").innerHTML="";
+    document.getElementById("row2").innerHTML="";
+    document.getElementById('incorrect').innerHTML="Incorrect: " + 0;
 
-function checkCard(){
 
-    //var altVal = this.alt;
-    if (this.id != checkId[0]){ // make sure they dont click the same card twice.
-
-        checkVal.push(this.alt);
-        checkId.push(this.id);
-
-        if (checkVal.length==1){
-            this.src = this.alt + ".jpg";
-
-        }
-        else if (checkVal.length ==2){
-            this.src = this.alt + ".jpg";
-        }
-
-        var card1 =checkVal[0];
-        var card2 =checkVal[1];
-
-        if (checkVal.length==2){
-
-            if (card1!=card2){
-
-                var myVar = setTimeout(myTimer, 1400);
-                        function myTimer() {
-                            document.getElementById(checkId[0]).src = "texture.jpg";
-                            document.getElementById(checkId[1]).src = "texture.jpg";
-                            checkId=[];
-                            checkVal=[];
-                        }
-
-            }
-            else{
-                checkId=[];
-                checkVal=[];
-            }
-
-        }
-
-    }
-
-}
-
-/*assigning the event listeners to the cards */
-
-var cards = document.getElementsByClassName("card");
-for (var x=0; x<cards.length; x++){
-    cards[x].addEventListener("click", checkCard);
+    gameStart();
 }
